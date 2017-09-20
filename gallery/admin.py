b07c0f31dd00130868fbccf769a316f4da655cd5
Nicolas Fernandez <nicojfernandez@gmail.com>
2017-09-20 18:20:16 -0300

    Cambios en Back_end

--- gallery/admin.py
+++ gallery/admin.py
@@ -1,6 +1,10 @@
 from django.contrib import admin
 from .models import Linea_Venta, Producto, Venta
+from gallery.models import AperturaCaja, Caja, Dia
 # Register your models here.
 admin.site.register(Linea_Venta)
 admin.site.register(Producto)
 admin.site.register(Venta)
+admin.site.register(Dia)
+admin.site.register(Caja)
+admin.site.register(AperturaCaja)
