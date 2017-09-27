6921e4257b629b95e92a8146262797164396ff33
JUAN-SAMSUNG\Juan <juan89nx@gmail.com>
2017-09-27 20:32:01 -0300

    no message

--- Bar7a/urls.py
+++ Bar7a/urls.py
@@ -1,7 +1,7 @@
 from django.conf.urls import include, url
 from django.contrib import admin
 
-from django.contrib.auth.views import LogoutView, LoginView
+from django.contrib.auth.views import *
 from django.contrib.auth import views as auth_views
 
 from back_end import views
