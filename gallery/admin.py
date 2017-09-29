ac7c0cc15803ba5173e1346b02b1fd1801a9e109
JUAN-SAMSUNG\Juan <juan89nx@gmail.com>
2017-09-28 21:34:25 -0300

    Nuevos Models para Reflejar, Impuestos y Precios por fecha

--- gallery/admin.py
+++ gallery/admin.py
@@ -1,12 +1,16 @@
 from django.contrib import admin
 from .models import Linea_Venta, Producto, Venta
-from gallery.models import AperturaCaja, Caja, Dia
+from gallery.models import *
 # Register your models here.
 admin.site.register(Linea_Venta)
 admin.site.register(Producto)
 admin.site.register(Venta)
-admin.site.register(Dia)
 admin.site.register(Caja)
+
+admin.site.register(ImpuestosProductoFecha)
+
+admin.site.register(PreciosProductoFecha)
+ 
 
 admin.site.register(AperturaCaja)
 
--- gallery/models.py
+++ gallery/models.py
@@ -1,11 +1,13 @@
 from django.db import models
 
+'''
 class Dia(models.Model):
     fecha_apertura = models.DateTimeField(auto_now_add=True, blank=True)
     fecha_cierre = models.DateTimeField(null=True, blank=True)
     Observaciones = models.CharField(max_length =250)
     def __str__(self):
         return self.fecha_apertura.strftime("%d-%m-%Y %H:%M:%S")
+'''
    
 class Caja(models.Model):
     Descripcion = models.CharField(max_length =250)
@@ -19,28 +21,50 @@ class AperturaCaja(models.Model):
     Caja = models.ForeignKey(Caja, on_delete = models.CASCADE)
     fecha_apertura_Caja = models.DateTimeField(auto_now_add=True, blank=True)
     fecha_cierre_Caja = models.DateTimeField(auto_now_add=True, blank=True)
-    Dia = models.ForeignKey(Dia, on_delete = models.CASCADE)
+#    Dia = models.ForeignKey(Dia, on_delete = models.CASCADE)
+
+class Producto(models.Model):
+    nombre = models.CharField(max_length =250)
+    observaciones = models.CharField(max_length =250)
+    foto = models.FileField()
+    aplica_impuesto = models.BooleanField()
+
+    def __str__(self):
+        return self.nombre,self.precio
+        
+class PreciosProductoFecha(models.Model):
+    Producto = models.ForeignKey(Producto, on_delete = models.CASCADE)
+    fecha_inicio = models.DateTimeField(auto_now_add=True, blank=True)
+    fecha_fin = models.DateTimeField(auto_now_add=True, blank=True)
+    precio_sin_iva = models.DecimalField(max_digits=15, decimal_places=2) 
+    
+    def __str__(self):
+        return self.Producto.nombre,self.fecha_inicio, self.fecha_fin, self.precio_sin_iva
+    
+class ImpuestosProductoFecha(models.Model):
+    Producto = models.ForeignKey(Producto, on_delete = models.CASCADE)
+    fecha_inicio = models.DateTimeField(auto_now_add=True, blank=True)
+    fecha_fin = models.DateTimeField(auto_now_add=True, blank=True)
+    porcentaje_impuesto = models.DecimalField(max_digits=15, decimal_places=2) 
+    
+    def __str__(self):
+        return self.Producto.nombre,self.fecha_inicio, self.fecha_fin, self.porcentaje_impuesto
     
 # Create your models here.
 class Venta(models.Model):
     AperturaCaja = models.ForeignKey(AperturaCaja, on_delete = models.CASCADE)
-    precio = models.DecimalField(max_digits=15, decimal_places=2)
     fecha = models.DateTimeField(auto_now_add=True, blank=True)
-    Observaciones = models.CharField(max_length =250)
-    cantidad_unidades = models.IntegerField(null = True)
+    total_sin_iva = models.DecimalField(max_digits=15, decimal_places=2)
+    monto_iva = models.DecimalField(max_digits=15, decimal_places=2)
+    total = models.DecimalField(max_digits=15, decimal_places=2)
+
     def __str__(self):
         return self.fecha,self.precio
     
-class Producto(models.Model):
-    precio = models.DecimalField(max_digits=15, decimal_places=2)
-    nombre = models.CharField(max_length =250)
-    observaciones = models.CharField(max_length =250)
-    foto = models.FileField()
-    def __str__(self):
-        return self.nombre,self.precio
-        
 class Linea_Venta(models.Model):
-    Producto = models.ForeignKey(Producto, on_delete = models.CASCADE)
+    PreciosProductoFecha = models.ForeignKey(PreciosProductoFecha, on_delete = models.CASCADE)
+    ImpuestosProductoFecha = models.ForeignKey(ImpuestosProductoFecha, on_delete = models.CASCADE)
     Venta = models.ForeignKey(Venta, on_delete = models.CASCADE)
+    
     def __str__(self):
-        return self.Venta, self.Producto
+        return self.Venta, self.PreciosProductoFecha
